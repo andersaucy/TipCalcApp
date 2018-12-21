00ccacbdf1c28c6c4dab0a5523c7a4b380a3d48a
Anderson Wang <atw293@nyu.edu>
2018-12-21 15:34:07 -0500

    Second Activity to sum up total payment per each person

---
+++ app/src/main/java/com/tiptip/andersaucy/tipcalculator/SecondActivity.java
@@ -0,0 +1,71 @@
+package com.tiptip.andersaucy.tipcalculator;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        final NumberFormat dollarFormat = NumberFormat.getCurrencyInstance(Locale.US);
+
+        if (getIntent().hasExtra("com.tiptip.andersaucy.tipcalculator.ADD")){
+            EditText dialogue = (EditText) findViewById(R.id.AMT);
+
+            EditText individual = (EditText) findViewById(R.id.INDIV);
+
+            EditText addText = (EditText) findViewById(R.id.addText);
+            addText.setKeyListener(null);
+            addText.setTextColor(Color.rgb(133, 187, 101));
+
+            String text = getIntent().getExtras().getString("com.tiptip.andersaucy.tipcalculator.ADD");
+            final double addend = Double.parseDouble(text);
+            addText.setText(dollarFormat.format(addend));
+
+            Button addBtn = (Button) findViewById(R.id.addBtn);
+            addBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText a1Text = (EditText) findViewById(R.id.a1);
+                    EditText a2Text = (EditText) findViewById(R.id.a2);
+                    EditText a3Text = (EditText) findViewById(R.id.a3);
+                    EditText a4Text = (EditText) findViewById(R.id.a4);
+                    TextView paymentText = (TextView) findViewById(R.id.payAmount);
+
+                    final String[] inputVal = {a1Text.getText().toString(),
+                                                a2Text.getText().toString(),
+                                                a3Text.getText().toString(),
+                                                a4Text.getText().toString()};
+
+                    double sum = numberize(inputVal);
+                    double payment = sum + addend;
+                    paymentText.setTextColor(Color.rgb(133, 187, 101));
+                    paymentText.setText(dollarFormat.format(payment));
+                   // instruction.setVisibility(View.VISIBLE);
+                   //next.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+    public double numberize (String[] inputVal){
+        double numberSum = 0.00;
+        for (int i = 0; i < inputVal.length; i++){
+            if (!inputVal[i].isEmpty()){
+                numberSum += Double.parseDouble(inputVal[i]);
+            }
+        }
+        return numberSum;
+    }
+}
